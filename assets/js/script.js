c77f7aec0d2c3ea45dc5d5d306337081840d12af
Fonkycat <40929307+Fonkycat@users.noreply.github.com>
2020-11-26 00:36:35 +0100

    Finalisation des élements obligatoires

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,36 +21,81 @@ carouselIntroduction.addEventListener('mouseleave', () => {
     setCarouselItemVisibility('none');
 
 });
+
 // focus catégorie
 let categoryList = document.getElementsByClassName('category'); // liste des catégories
 let categoryLinkList = document.getElementsByClassName('categoryLink'); // liste des liens-catégorie
-for ( const element of categoryLinkList) {
+for (const element of categoryLinkList) {
     element.addEventListener('click', showCategory)
 }
 
-function showCategory(){
+function showCategory() {
     let home = document.getElementById('home');
     home.style.display = 'none';
     // disparition de la home 
-    let categoryPosition = this.href.indexOf('#')+1; // récupération de la position du caractère après le # dans le lien qui a déclenché la fonction
+    let categoryPosition = this.href.indexOf('#') + 1; // récupération de la position du caractère après le # dans le lien qui a déclenché la fonction
     let category = this.href.substring(categoryPosition); // récupération de la chaine de caractère après le # (= nom de la catégorie)
     for (const element of categoryList) {
-        if (element.id == category){
-            element.style.display = 'block'
-        }else {
-            element.style.display = 'none'
+        if (element.id == category) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
         }
-        };
-    
-}
-function addToCart(btn){
-    let addItem = btn.parentElement.parentElement.getElementsByTagName('h2')[0].getAttribute('data-name'); //on récupère le nom de l'article
-    let cartContent= document.getElementById('cartContent'); // ciblage de la modal
-    let addRef = btn.parentElement.parentElement.getElementsByClassName('reference')[0].textContent; //récupération de la ref article
-    let addPrice = btn.parentElement.getElementsByClassName('price')[0].getAttribute('data-price'); // récupération du prix de l'article
-    cartContent.innerHTML = addItem + " " + ":" + " " + addRef + '<button class="btn">-</button><p class="itemQuantity">' + '5' + '</p><button class="btn">+</button>' + addPrice + '€'; // création de la ligne produit dans la modal (nom, ref, boutons, quantité, prix)
+    };
 
-    
 }
 
+//panier
+// les articles dans le panier sont sous la forme NOM_REF_NB_PRIX
+let products = [];
 
+// ajouter au panier
+function addToCart(btn) {
+    let item = btn.parentElement.parentElement.getElementsByTagName('h2')[0].getAttribute('data-name'); //on récupère le nom de l'article
+    let reference = btn.parentElement.parentElement.getElementsByClassName('reference')[0].textContent; //récupération de la ref article
+    let price = btn.parentElement.getElementsByClassName('price')[0].getAttribute('data-price'); // récupération du prix de l'article
+    let productInCart = false;
+
+    // vérifie si le produit est déjà dans le panier
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].split('_')[1] === reference) {
+            productInCart = true; // le produit est dans le panier
+            let splitted = products[i].split('_'); // découpe et stock chaque élément du produit (nom, ref, nb, prix)
+            products[i] = splitted[0] + '_' + splitted[1] + '_' + (parseFloat(splitted[2]) + 1) + '_' + splitted[3];
+        }
+    }
+
+    // si le produit n'est pas dans le panier, l'ajouter
+    if (productInCart === false) {
+        products.push(item + '_' + reference + '_' + '1' + '_' + price);
+    }
+
+    updateCart();
+}
+
+// Met à jour la modal du panier
+function updateCart() {
+    let cartContent = document.getElementById('cartContent'); // ciblage du contenu de la modal
+    cartContent.innerHTML = '';
+    let totalPrice = 0;
+    for (let i = 0; i < products.length; i++) {
+        let splitted = products[i].split('_'); 
+        totalPrice += splitted[2] * splitted[3]; //Ajout du prix des articles fois la quantité, dans le total 
+        cartContent.innerHTML += splitted[0] + ' : ' + splitted[1] + '<button data-ref="' + splitted[1] + '" onclick="addItem(this, -1)" class="btn">-</button><p class="itemQuantity">' + splitted[2] + '</p><button data-ref="' + splitted[1] + '" onclick="addItem(this, 1)" class="btn">+</button><p>' + (splitted[2] * splitted[3]).toFixed(2) + '€'; // création de la ligne produit dans la modal (nom, ref, boutons, quantité, prix)
+    }
+    cartContent.innerHTML += '<p>Total : ' + totalPrice.toFixed(2) + '€</p>';
+}
+
+// boutons + et -
+function addItem(btn, amount) {
+    let reference = btn.getAttribute('data-ref');
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].split('_')[1] === reference) {
+            let splitted = products[i].split('_');
+            products[i] = splitted[0] + '_' + splitted[1] + '_' + (parseFloat(splitted[2]) + amount) + '_' + splitted[3];
+        }
+    }
+
+    updateCart();
+}
