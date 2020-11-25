abddc67c7ff46c961db9be688b0195bb72f0e9dc
unknown <pierre-antoine-cantenot@outlook.fr>
2020-11-25 12:00:16 +0100

    Modification carousel et navbar

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const carouselIntroduction = document.getElementById('carouselExampleIndicators');
+let carouselText = document.querySelectorAll('.carouselText');
+let carouselButton = document.querySelectorAll('.carouselButton');
+
+function setCarouselItemVisibility(show) {
+    for (let i = 0; i < carouselText.length; i++) {
+        carouselText[i].style.display = show;
+    }
+
+    for (let i = 0; i < carouselButton.length; i++) {
+        carouselButton[i].style.display = show;
+    }
+}
+
+carouselIntroduction.addEventListener('mouseenter', () => {
+    setCarouselItemVisibility('block');
+});
+
+carouselIntroduction.addEventListener('mouseleave', () => {
+    setCarouselItemVisibility('none');
+
+});
